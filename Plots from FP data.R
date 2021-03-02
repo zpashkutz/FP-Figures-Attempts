e8919cd6eb81c44370d7bcbaacb220bf76f257b0
Andrew Saghir <asaghir@uci.edu>
2021-03-02 10:04:21 -0800

    changes filtering/rarefaction in plots, added intro to rmd, and added html of the Rmd

--- Plots from FP data.R
+++ Plots from FP data.R
@@ -10,15 +10,21 @@ library(biomformat)
 library(stringr)
 library(plyr)
 library(gridExtra)
-library(readxl)
+library(readxl) #maybe not this one
 
 #import metadata
-metadataFP <- read.delim("Microbiome_mapping_14Oct2019.txt")
+metadataFP <- read.delim("Microbiome_mapping_14Oct2019.txt", sep = "")
 str(metadataFP)
 #add X in front of sample in metadataFP
 metadataFP$X.NAME <- interaction( "X", metadataFP$X.NAME, sep = "")
 rownames(metadataFP) <- metadataFP[,1]
 metadataFP[,1] <- NULL 
+
+
+##md <- metadataFP
+##names <- rownames(md)
+##rownames(md) <- NULL
+##newmd <- cbind(names, md)
 
 
 #get taxonomy - so functionally this will be equivalent to OTU_taxalevel from the Alex Chase tutorial 
@@ -37,42 +43,49 @@ table(OTU.taxonomyFP$Genus)
 #import OTU table and specify that the first column contains the row names 
 OTU.tableFP <- read.delim("OTU_table.txt", row.names = 1)
 
-#merge OTU taxonomy with OTU taxa level to filter? pls werk
+#merge OTU taxonomy with OTU taxa level to filter?
 OTU.table.with.taxonomyFP <- as.data.frame(merge(OTU.taxonomyFP, 
                                                OTU.tableFP, by.x = "row.names", by.y = "row.names"))
-#why did this take me so long... 
 
 #OMITTING FILTERING BEFORE RAREFACTION undo "##" to enable filtering 
 ##################################################################
 #Filtering Taxa - lets try this by omitting filtering and going straight to rarefaction
-OTU.table.filteredtaxaFP <- OTU.table.with.taxonomyFP[!grepl("Unassigned|chloroplast", 
+
+OTU.table.filteredtaxaFP <- OTU.table.with.taxonomyFP[!grepl("Unassigned", 
                                                          OTU.table.with.taxonomyFP$Kingdom),]
 #subset only OTU table with the filtered taxa
 FPfilteredtaxa <- OTU.table.filteredtaxaFP$Row.names
 FPfilterOTU <- subset(OTU.tableFP, rownames(OTU.tableFP) %in% FPfilteredtaxa)
-FPfinalOTU <- FPfilterOTU[,!names(FPfilterOTU) %in% c("Mock")]
+
+#For us the below line doesn't do anything but, if there were any mock community in qiita and qiime2 we should have filtered them out - to my understanding
+#FPfinalOTU <- FPfilterOTU[,!names(FPfilterOTU) %in% c("Mock")]
 
 # get quartile ranges for rarefaction
 FPtransOTU <- rowSums(t(OTU.tableFP)) 
+Q0 <- quantile(FPtransOTU[order(FPtransOTU, decreasing = TRUE)], 0.0)
 Q10 <- quantile(FPtransOTU[order(FPtransOTU, decreasing = TRUE)], 0.10)
 Q15 <- quantile(FPtransOTU[order(FPtransOTU, decreasing = TRUE)], 0.15)
-
+Q25 <- quantile(FPtransOTU[order(FPtransOTU, decreasing = TRUE)], 0.25)
 #look at ranges
 barplot(sort(FPtransOTU), ylim = c(0, max(FPtransOTU)), 
         xlim = c(0, NROW(FPtransOTU)), col = "Blue", ylab = "Read Depth", xlab = "Sample") 
-abline(h = c(Q10, Q15), col = c("red", "pink"))
+abline(h = c(Q0, Q10, Q15,Q25), col = c("green","red", "pink", "cyan"))
 plot.new()
 
 ######################### Right here is where I think I messed up, the samples got cut in size from 95 to 89
 #lines here did not plateau meaning that we did not sequence the entirety of the community 
 #because of this I'm going to keep everything in the 90th percentile, so to the left of the red line 
 rarecurve(t(OTU.tableFP), step = 100, cex = 0.5)
-abline(v = c(Q10, Q15), col = c("red", "pink"))
+abline(v = c(Q0, Q10, Q15, Q25), col = c("green","red", "pink", "cyan"))
 
 #deciding to keep everything to the left of the red line in the rarefied OTU table 
-FPrared.OTU <- as.data.frame((rrarefy.perm(t(OTU.tableFP), sample = Q10, n = 100, round.out = T)))
-#only keep what meets the rarefaction cutoff
-FPrared.OTU <- as.data.frame(FPrared.OTU[rowSums(FPrared.OTU) >= Q10 - (Q10 * 0.1), colSums(FPrared.OTU) >= 1]) 
+FPrared.OTU <- as.data.frame(rrarefy.perm(t(OTU.tableFP)), sample = Q0, n = 100, round.out = T)
+
+raredreads <- as.data.frame(rowSums(FPrared.OTU))
+colnames(raredreads) [1] <- "read count"
+#only keep what meets the rarefaction cutoff -  so don't do this then?   This function throws out all samples 
+#to the left of the chosen line
+#FPrared.OTU <- as.data.frame(FPrared.OTU[rowSums(FPrared.OTU) >= Q0 - (Q0 * 0.1), colSums(FPrared.OTU) >= 1]) 
 
 #Lets look at this OTU to see if we messed it up 
 View(FPrared.OTU)
@@ -130,35 +143,49 @@ adonis(data = filtermetaFP, formula = FPrared.OTU ~ Family/ Individual/
 #plot the actual NMDS 
 ggplot(data = nmds.fam.metadata) +
   aes(x = MDS1, y = MDS2, color = Factor.F) + #Creates and colors legend to match, modify after $.
-  geom_point() +
+  geom_point() + 
   labs(col = "Family") + #Renames legend, modifiable within quotes.
-  ggtitle("NMDS of Families' Oral Microbiomes", subtitle = bquote(~R^2~ '= 0.199, p = 0.001')) + #Adds tittle and subtitle. Can modify p and r-squared values + title.
-  theme_classic(base_size = 14, base_line_size = .5)
+  ggtitle("NMDS of Families' Oral Microbiomes", subtitle = bquote(~R^2~ '= 0.057, p = 0.006')) + #Adds tittle and subtitle. Can modify p and r-squared values + title.
+  theme_classic(base_size = 14, base_line_size = .5) +
+  stat_ellipse(size=1)
+
+
+###############
+###############
+###############
 
 #plot the actual NMDS - This one should be exactly the same as the one above
 #Here I'm going to try to use ordiellipse to make the ellipses - idk if it's gonna work tho
 
-ord <-ordiellipse(NMDS.fam, nmds.fam.metadata$Family, display = "sites", 
-                kind = "sd", conf = 0.95, label = F)
+#Okay so this catastrophically failed, I'll keep this in here for now to revisit -  try different methods for now
+# all the lines with "###" in front designate code for the failed ordiellipse attempt
 
-df_ell <- data.frame()
-  for(g in levels(nmds.fam.metadata$Family)){
-    df_ell <- rbind(df_ell, cbind(as.data.frame(with(nmds.fam.metadata[nmds.fam.metadata$Family==g,],
-                                                    veganCovEllipse(ord[[g]]$cov,ord[[g]]$center,ord[[g]]$scale)))
-                                 ,Factor.F=g))
-}
+###############
+###############
+###############
+
+###ord <-ordiellipse(NMDS.fam, nmds.fam.metadata$Family, display = "sites", 
+             ###   kind = "sd", conf = 0.95, label = F)
+
+###df_ell <- data.frame()
+###  for(g in levels(nmds.fam.metadata$Family)){
+  ###  df_ell <- rbind(df_ell, cbind(as.data.frame(with(nmds.fam.metadata[nmds.fam.metadata$Family==g,],
+   ###                                                 veganCovEllipse(ord[[g]]$cov,ord[[g]]$center,ord[[g]]$scale)))
+                  ###               ,Factor.F=g))
+###}
 
 
-ggplot(data = nmds.fam.metadata) +
-  aes(x = MDS1, y = MDS2, color = Factor.F) + #Creates and colors legend to match, modify after $.
-  geom_point(aes(color = Factor.F)) + geom_path(data = df_ell, aes(x=MDS1, y=MDS2, color=Factor.F), size =1, linetype=1) +
-  labs(col = "Family") + #Renames legend, modifiable within quotes.
-  ggtitle("NMDS of Families' Oral Microbiomes", subtitle = bquote(~R^2~ '= 0.199, p = 0.001')) + #Adds tittle and subtitle. Can modify p and r-squared values + title.
-  theme_classic(base_size = 14, base_line_size = .5)
+###ggplot(data = nmds.fam.metadata) +
+ ### aes(x = MDS1, y = MDS2, color = Factor.F) + #Creates and colors legend to match, modify after $.
+###  geom_point(aes(color = Factor.F)) + geom_path(data = df_ell, aes(x=MDS1, y=MDS2, color=Factor.F), size =1, linetype=1) +
+ ### labs(col = "Family") + #Renames legend, modifiable within quotes.
+ ### ggtitle("NMDS of Families' Oral Microbiomes", subtitle = bquote(~R^2~ '= 0.199, p = 0.001')) + #Adds tittle and subtitle. Can modify p and r-squared values + title.
+  ###theme_classic(base_size = 14, base_line_size = .5)
+
+
 
 #Make some alpha diversity plots and see if they line up with what I can generate from MicrobiomeAnalyst 
 #Originally had FPrared.OTU (2 instances below) in place of OTU.tableFP because rarefied values were used, note you must transpose the "OTU.tableFP to use" 
-
 FPrichness <- as.data.frame(specnumber(FPrared.OTU))
 colnames(FPrichness) <- c("speciesrich")
 #Merge with metadata for plot
